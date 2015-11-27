51c31078c24317d624b395687d7b4a56215c9d2d
ButenkoT <tanya@butenko.me>
2015-11-27 12:59:05 +1100

    rename bin folder to server

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/public_chat', publicChat);
+app.use('/public-chat', publicChat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/publicChat.js
+++ routes/publicChat.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET chat page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('publicChat');
 });
 
